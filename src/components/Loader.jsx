720af2061959fb047918635addaec9b5af189b85
Sooriya <sooriyanethaji03@gmail.com>
2026-02-16 15:38:05 +0530

    fix: responsive loader is added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,10 +4,16 @@ function Loader() {
       <div className="relative flex items-center justify-center">
         
         {/* Spinning Ring */}
-        <div className="w-28 h-28 border-4 border-gray-200 border-t-black rounded-full animate-spin"></div>
+        <div className="
+          w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28
+          border-4 border-gray-200 border-t-black rounded-full animate-spin
+        "></div>
 
         {/* Center Text */}
-        <div className="absolute text-2xl font-bold text-gray-900 tracking-wide">
+        <div className="
+          absolute text-xl sm:text-2xl md:text-3xl lg:text-4xl 
+          font-bold text-gray-900 tracking-wide
+        ">
           Gidy
         </div>
 
